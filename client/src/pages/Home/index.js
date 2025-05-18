8afa7d70a6db20d570027850551396cc21e93ef1
MSI\ANH THU <truongthianhthu2011@gmail.com>
2025-05-18 21:44:52 +0700

    feat (FE): movie topics grid (topic page)

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import MovieBanner from "~/components/layout/components/Movies/MovieBanner";
+import MovieTopicGrid from "~/components/layout/components/Movies/MovieTopicGrid";
 
 const Home = () => {
   return (
     <div>
       <MovieBanner />
+      <MovieTopicGrid />
     </div>
   );
 };
